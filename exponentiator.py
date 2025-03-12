3ff544a7e7d780aa4d50aca8088ee31bafa07c41
Angelika Brown <angelikpbrown@gmail.com>
2025-03-12 07:24:12 -0400

    Add files via upload
    Finished.

---
+++ exponentiator.py
@@ -0,0 +1,17 @@
+
+def exponentiate(x, y):
+        return x ** y
+
+def raise_to_fourth_power(x):
+      return exponentiate(x, 4)
+
+square = lambda x: x ** 2
+
+cube = lambda x: x ** 3
+
+print(square(2))
+print(cube(2))
+print(raise_to_fourth_power(2))
+
+
+
---
+++ greet.py
@@ -0,0 +1,12 @@
+def greet(name):
+	print("Hi " + name)
+
+def name_input():
+	name = input("Enter name:")
+	greet(name)
+	
+
+name_input()
+
+
+
